3e752eeb2a56fd0549acf2cc0cf5fa8d94e59c6b
thatguy <xarodasimba@gmail.com>
2019-09-12 15:26:03 +0300

    initial commit. connected to firebase cloude database Added a helpers.js file to debounce requests to the database

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const firebase = require("firebase");
+require('firebase/firestore');
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDkhz959A1e9-xST4z6nxi_GKdCKZzansc",
+    authDomain: "nevernote-d851f.firebaseapp.com",
+    databaseURL: "https://nevernote-d851f.firebaseio.com",
+    projectId: "nevernote-d851f",
+    storageBucket: "nevernote-d851f.appspot.com",
+    messagingSenderId: "435283328945",
+    appId: "1:435283328945:web:8ac974254a60ed9d914790"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+ReactDOM.render(<App />, document.getElementById('nevernote-container'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
