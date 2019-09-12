ad8af9fe9919c92f0b958651478712a0762aec7b
thatguy <xarodasimba@gmail.com>
2019-09-12 18:46:52 +0300

    refactored some code

--- src/index.js
+++ src/index.js
@@ -3,20 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import { firebaseConfig } from './creds'
 const firebase = require("firebase");
 require('firebase/firestore');
 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDkhz959A1e9-xST4z6nxi_GKdCKZzansc",
-    authDomain: "nevernote-d851f.firebaseapp.com",
-    databaseURL: "https://nevernote-d851f.firebaseio.com",
-    projectId: "nevernote-d851f",
-    storageBucket: "nevernote-d851f.appspot.com",
-    messagingSenderId: "435283328945",
-    appId: "1:435283328945:web:8ac974254a60ed9d914790"
-};
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
